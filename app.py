efeb4dba38ed9928a16ab4167337454fd846cec2
Lucas-C3p <Lucascorrea0408@gmail.com>
2026-06-04 18:25:32 -0300

    Atualiza descrição da página sobre

--- app.py
+++ app.py
@@ -14,5 +14,10 @@ def status():
 def home():
     return "Sistema de Gerenciamento de Biblioteca"
 
+@app.route("/sobre")
+def sobre():
+    return "Sistema desenvolvido em Flask para estudo de CI/CD"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
