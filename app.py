06834df04aec5dce668c07bac996929959842010
Lucas-C3p <Lucascorrea0408@gmail.com>
2026-06-09 10:12:10 -0300

    chore: prepara aplicação para deploy

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os 
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -39,5 +40,5 @@ def formulario_livro():
     return "Formulário de cadastro de livros"
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
